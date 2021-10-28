486e7c516de9fae122ad8b273b1768eabcde2bd8
rsbyrne <rohan.byrne@gmail.com>
2021-10-28 05:22:39 +0000

    Quick push

--- push.sh
+++ push.sh
@@ -4,7 +4,6 @@ cd "$(dirname "$0")"
 bash configure_ssh.sh
 eval "$(ssh-agent)"
 ssh-add ~/.ssh/*.pem
-python3 -m nbconvert --clear-output working/*.ipynb
 git config --global user.email "rohan.byrne@gmail.com"
 git config --global user.name "rsbyrne"
 git add .
