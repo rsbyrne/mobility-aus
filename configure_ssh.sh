e4b932a29e23d4fdade3b4bf495f37869b5461e7
rsbyrne <rohan.byrne@gmail.com>
2022-01-17 03:31:48 +0000

    Quick push

--- configure_ssh.sh
+++ configure_ssh.sh
@@ -2,12 +2,11 @@
 currentDir=$PWD
 cd "$(dirname "$0")"
 mkdir ~/.ssh/
-sudo rm -r ~/.ssh/*
+#sudo rm -r ~/.ssh/*
 cp -f *.pem ~/.ssh/
 sudo chmod 700 ~/.ssh/
-sudo chmod 600 ~/.ssh/*
-# eval "$(ssh-agent)"
-# ssh-add ~/.ssh/*.pem
+sudo chmod 600 ~/.ssh/*.pem
+eval "$(ssh-agent)" && ssh-add ~/.ssh/*.pem
 git config --global url.ssh://git@github.com/.insteadOf https://github.com/
 echo 'StrictHostKeyChecking no' > ~/.ssh/config
 cd $currentDir
